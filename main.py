92ad76f44ecfaed1e710c64ce5835d7cbf4cbfd4
Joseph Ferguson <jjferguson1@sheffield.ac.uk>
2025-07-27 21:01:45 +0100

    memory

--- main.py
+++ main.py
@@ -2,26 +2,35 @@ import ollama
 import subprocess
 import os
 
+messages = [
+    {"role": "system", "content": "You are an ai in the windows command prompt, assist the user by responding and or using the appropriate command prompt. To use a command prompt put it square brackets, only use square brackets for a valid and safe command prompt. When more information is needed, use command prompts to find it or ask the user. You must never open command prompt for the user, only you can run commands in command prompt. Keep responses short and concise."},
+]
+
 while True:
     # Displays the cwd
     cwd = os.getcwd()
+    
+    # Saves user response
     user_input = input(cwd + ">")
+    messages.append({"role": "user", "content": user_input})
 
-    messages = [
-        {"role": "system", "content": "You are an ai in the windows command prompt, assist the user by responding and or using the appropriate command prompt. To use a command prompt put it square brackets. When more information is needed, use command prompts to find it or ask the user. You must never open command prompt for the user, only you can run commands in command prompt."},
-        {"role": "user", "content": user_input}
-    ]
-
+    # Generates ai response
     response = ollama.chat(model='gemma3', messages=messages)
     response = response['message']['content']
+    messages.append({"role": "assistant", "content": response}) # Saves ai response
     print(response)
 
     try:
+        # Runs command prompt
         start = response.index('[')
         end = response.index(']')
         command = response[start + 1:end]
         result = subprocess.run(command, shell=True, capture_output=True, text=True)
+        
+        # Allows ai to see command prompt
+        messages.append({"role": "user", "content": "this is the result of the command prompt: " + result.stdout + " " + result.stderr})
         print(result.stdout)
         print(result.stderr)
+   
     except ValueError:
         pass
